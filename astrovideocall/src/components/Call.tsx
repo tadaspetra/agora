bf3e3025119e15104b4287a5387eb1490c3200a3
Tadas Petra <tadas@tadaspetra.com>
2023-06-29 09:24:36 -0500

    update readme

--- astrovideocall/src/components/Call.tsx
+++ astrovideocall/src/components/Call.tsx
@@ -25,7 +25,7 @@ const Call = (props:AppInfo) => {
       <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} /> 
     </div>
   ) : (
-    <div className='ml-2 flex flex-col'>
+    <div className='ml-12 flex flex-col'>
       <div className='pt-10'></div>
       <button className="px-5 py-3 mt-5 text-base font-medium text-center text-white bg-gray-400 rounded-lg hover:bg-gray-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-40" onClick={() => setVideoCall(true)}>Rejoin Call</button>
       <a className=" px-5 py-3 mt-5 text-base font-medium text-center text-white bg-blue-400 rounded-lg hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-40" href='/'>Back</a>
